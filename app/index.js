802900c06bcf017a392def1d56f5bfc6ce20a56b
Vaishnav_Tej64 <vaishnavtej522@gmail.com>
2026-04-21 15:10:40 +0530

    test pipeline

--- app/index.js
+++ app/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Hello DevSecOps 🚀");
+    res.send("Hello DevSecOps AUTO DEPLOY SUCCESS 🔥🚀");
 });
 
 app.listen(3000, () => {
